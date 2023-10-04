1e4d1ef8aa9bb37fa0a47282146c4327ec2ad7d7
stamayo <sebastian18t@gmail.com>
2023-10-03 22:00:56 -0500

    scrip inicial

---
+++ analisis exploratio de datos.R
@@ -0,0 +1,8 @@
+#------------------------------------------------------#
+#-- Universidad del Valle: Escuela de Estadistica    --#
+#-- Asignatura: Probabilidad y Estadística           --#
+#-- Estudiantes: J.J MAFLA 2126990 - S.Tamayo 2110331--#   
+#------------------------------------------------------#
+
+##### Analisis exploratorio del peso #####
+
